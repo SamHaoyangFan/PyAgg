def0b462ca187bbdea26e4462db31987c59a4009
Sam Fan <samfanperiod2@gmail.com>
2022-07-23 15:10:30 -0700

    Added files and changes required for deployment to heroku

--- content_aggregator/content_aggregator/settings.py
+++ content_aggregator/content_aggregator/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxsd23')
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-(5yecxiwy#o0y)-7qbmgk&r8av!w6%yw3tm=9u=9_xdy3ui6rh')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(5yecxiwy#o0y)-7qbmgk&r8av!w6%yw3tm=9u=9_xdy3ui6rh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
